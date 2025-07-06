52cb6154e18af810f0758de73997075a9ba91f0e
jianglh <jianglianhui2008@gmail.com>
2025-07-06 08:23:33 +0800

    fix: 彻底修复Hive TypeId冲突问题 - 使用独立的TypeId范围

--- lib/types/document.dart
+++ lib/types/document.dart
@@ -4,7 +4,7 @@ import 'package:hive/hive.dart';
 part 'document.g.dart';
 
 /// 文档类型枚举
-@HiveType(typeId: 2)
+@HiveType(typeId: 10)
 enum DocumentType {
   @HiveField(0)
   markdown('md', '普通Markdown文档'),
@@ -22,7 +22,7 @@ enum DocumentType {
 }
 
 /// 文档实体类
-@HiveType(typeId: 3)
+@HiveType(typeId: 11)
 class Document extends Equatable {
   const Document({
     required this.id,
